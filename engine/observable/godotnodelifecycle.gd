3bd3dc83a4d426eed0726ba65ee2e2277f01dad7
Raoul Zebisch <neroware@freenet.de>
2022-09-16 14:34:34 +0200

    Improved Node Lifecycle Observables not added node per subscribe

--- engine/observable/godotnodelifecycle.gd
+++ engine/observable/godotnodelifecycle.gd
@@ -25,37 +25,43 @@ class _ListenerOnUnhandledKeyInput extends _NodeLifecycleListener:
 	func _unhandled_key_input(event : InputEvent):
 		on_event.emit(event)
 
-## Represents [Node] lifecycle events like [code]_process(delta)[/code]. 
+## Represents [Node] lifecycle events like [method Node._process].
 ## Observable emits argument from call as item on the stream.
 ## [br][br]
 ## [color=yellow]Warning![/color] This only creates a Node of type [b]_NodeLifecycleListener[/b]
 ## which is added as a child since it is not possible to get signals on lifecycle callbacks.
 static func from_godot_node_lifecycle_event_(conn : Node, type : int) -> Observable:
+	var listener : _NodeLifecycleListener = null
+	var count : RefValue = RefValue.Set(0)
+	
 	var subscribe = func(
 		observer : ObserverBase,
 		scheduler : SchedulerBase = null
 	) -> DisposableBase:
-		var _listener : _NodeLifecycleListener
-		match type:
-			0:
-				_listener = _ListenerOnProcess.new()
-			1:
-				_listener = _ListenerOnPhysicsProcess.new()
-			2:
-				_listener = _ListenerOnInput.new()
-			3:
-				_listener = _ListenerOnShortcutInput.new()
-			4:
-				_listener = _ListenerOnUnhandledInput.new()
-			5:
-				_listener = _ListenerOnUnhandledKeyInput.new()
-		_listener.name = "__" + str(conn.name) + "$listener__"
-		conn.call_deferred("add_child", _listener)
+		if count.v == 0:
+			match type:
+				0:
+					listener = _ListenerOnProcess.new()
+				1:
+					listener = _ListenerOnPhysicsProcess.new()
+				2:
+					listener = _ListenerOnInput.new()
+				3:
+					listener = _ListenerOnShortcutInput.new()
+				4:
+					listener = _ListenerOnUnhandledInput.new()
+				5:
+					listener = _ListenerOnUnhandledKeyInput.new()
+			listener.name = "_GDRxListener" + str(conn.get_instance_id())
+			conn.call_deferred("add_child", listener)
+		count.v += 1
 		
 		var dispose = func():
-			_listener.queue_free()
+			count.v -= 1
+			if count.v == 0:
+				listener.queue_free()
 		
-		var subscription = GDRx.gd.from_godot_signal(_listener.on_event).subscribe(
+		var subscription = GDRx.gd.from_godot_signal(listener.on_event).subscribe(
 			observer, func(e):return, func():return,
 			scheduler
 		)
