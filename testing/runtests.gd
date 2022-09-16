34829e9f2d3e8d9b51065f135cd97c87245ea17b
Raoul Zebisch <neroware@freenet.de>
2022-09-16 17:33:20 +0200

    Rewrote test suit

--- testing/runtests.gd
+++ testing/runtests.gd
@@ -1,173 +1,177 @@
 extends Node
 
+@export var tests : String = "map"
+
 enum ETestState {
-	RUNNING = 0,
 	SUCCESS = 1,
-	FAILED = -1
+	FAILED = 0,
+	MISSING = -1
 }
 
-signal on_assert(test_id, succ)
+signal sequence_finished(result : ETestState, remaining : int)
 
-func _assert(pred : bool, test_id : int, succ : Array[ETestState], succ_idx : int = 0):
-	if pred: succ[succ_idx] = ETestState.SUCCESS
-	else: succ[succ_idx] = ETestState.FAILED
-	emit_signal("on_assert", test_id, succ)
-
-func _fail(test_id : int, succ : Array[ETestState], succ_idx : int = 0):
-	succ[succ_idx] = ETestState.FAILED
-	emit_signal("on_assert", test_id, succ)
-
-func _success(test_id : int, succ : Array[ETestState], succ_idx : int = 0):
-	succ[succ_idx] = ETestState.SUCCESS
-	emit_signal("on_assert", test_id, succ)
-
-func _launch_next_test(test_id : int, test_callbacks : Array, n_succ = 0, n_fail = 0):
-	if test_id >= test_callbacks.size():
-		print("Finished all tests!")
-		print("FAILED: ", n_fail)
-		print("SUCCESS: ", n_succ)
-	else:
-		#print("[ReactiveX]: '", test_callbacks[test_id] + "'")
-		print("[ReactiveX]: " + test_callbacks[test_id].substr(6), " (", (test_id + 1), " / ", test_callbacks.size(), ")")
-		call(test_callbacks[test_id], test_id)
+class ObservableSequence extends ArrayIterator:
+	class Error:
+		pass
+	class Complete:
+		pass
+	class UnsubscribeOn:
+		var value = null
+		func _init(value):
+			self.value = value
+	
+	var seq : Array
+	
+	func _init(seq : Array):
+		self.seq = seq
+		super._init(seq)
+	
+	func count_internal_sequences() -> int:
+		var count = 0
+		for elem in self.seq:
+			if elem is ObservableSequence.Complete:
+				break
+			elif elem is ObservableSequence.Error:
+				break
+			elif elem is ObservableSequence.UnsubscribeOn:
+				if elem.value is ObservableSequence:
+					count = count + 1 + elem.value.count_internal_sequences()
+				break
+			elif elem is ObservableSequence:
+				count += 1 + elem.count_internal_sequences()
+		return count
+	
+	func compare(obs : Observable, result : Signal, remaining : RefValue = null):
+		var seq_remaining : RefValue = RefValue.Set(1 + self.count_internal_sequences()) if remaining == null else remaining
+		
+		var end_sequence = func(state : ETestState):
+			seq_remaining.v = seq_remaining.v - 1 if state == ETestState.SUCCESS else -1
+			result.emit(state, seq_remaining.v)
+		
+		var cmp = func(expected, check) -> ETestState:
+			if expected is ObservableSequence and check is Observable:
+				expected.compare(check, result, seq_remaining)
+				return ETestState.SUCCESS
+			if expected == check:
+				return ETestState.SUCCESS
+			print("[ReactiveX]: Equality check failed: ", expected, " != ", check, ".")
+			return ETestState.FAILED
+		
+		var on_next = func(i):
+			if seq_remaining.v <= 0:
+				return
+			
+			var expected = self.next()
+			if expected is Complete:
+				print("[ReactiveX]: Expected end of sequence but got item!")
+				end_sequence.call(ETestState.FAIL)
+			
+			elif expected is Error:
+				print("[ReactiveX]: Expected error in sequence but got item!")
+				end_sequence.call(ETestState.FAIL)
+			
+			elif expected is UnsubscribeOn:
+				end_sequence.call(cmp.call(expected.value, i))
+			
+			elif cmp.call(expected, i) == ETestState.FAILED:
+				end_sequence.call(ETestState.FAILED)
+		
+		var on_error = func(__):
+			if seq_remaining.v <= 0:
+				return
+			
+			var expected = self.next()
+			if expected is ObservableSequence.Error:
+				end_sequence.call(ETestState.SUCCESS)
+			else:
+				print("[ReactiveX]: Expected error as end of sequence!")
+				end_sequence.call(ETestState.FAILED)
+		
+		var on_completed = func():
+			if seq_remaining.v <= 0:
+				return
+			
+			var expected = self.next()
+			if expected is ObservableSequence.Complete:
+				end_sequence.call(ETestState.SUCCESS)
+			else:
+				print("[ReactiveX]: Expected end of sequence!")
+				end_sequence.call(ETestState.FAILED)
+		
+		obs.subscribe(on_next, on_error, on_completed)
 
 func _ready():
-	var test_callbacks = []
-	for m in get_method_list():
-		if not m["name"].begins_with("_test_"):
-			continue
-		test_callbacks.append(m["name"])
-
-	var on_assert = GDRx.FromGodotSignal(self.on_assert)
-	var current_test = RefValue.Set(0)
-
-	var success_count = RefValue.Set(0)
-	var fail_count = RefValue.Set(0)
-
-	on_assert.subscribe(
-		func(t : Tuple):
-			var test_id : int = t.at(0)
-			var succ : Array[ETestState] = t.at(1)
-			if test_id != current_test.v:
-				return
-
-			if succ.all(func(elem): return elem == ETestState.SUCCESS):
-				print("[ReactiveX]: Success\n")
-				success_count.v += 1
-				current_test.v += 1
-				_launch_next_test(current_test.v, test_callbacks, success_count.v, fail_count.v)
-			elif succ.any(func(elem): return elem == ETestState.FAILED):
-				print("[ReactiveX]: Failed\n")
-				fail_count.v += 1
-				current_test.v += 1
-				_launch_next_test(current_test.v, test_callbacks, success_count.v, fail_count.v)
-	)
-	print("[ReactiveX]: Running tests . . .\n")
-	_launch_next_test(current_test.v, test_callbacks)
-
-func _test_op_all(test_id : int):
-	var succ : Array[ETestState] = [0, 0]
-
-	var stream = GDRx.FromRange(0, 100, 3).pipe1(
-		GDRx.op.all(func(i : int): return i % 3 == 0)
-	)
-	var v1 = RefValue.Set(false)
-	stream.subscribe(
-		func(b : bool): v1.v = b,
-		func(__): _fail(test_id, succ, 0),
-		func(): _assert(v1.v, test_id, succ, 0)
-	)
-	var stream2 = GDRx.FromRange(0, 100, 3).pipe1(
-		GDRx.op.all(func(i : int): return i % 2 == 0)
-	)
-	var v2 = RefValue.Set(false)
-	stream2.subscribe(
-		func(b : bool): v2.v = not b,
-		func(__): _fail(test_id, succ, 1),
-		func(): _assert(v2.v, test_id, succ, 1)
-	)
-
-func _test_op_amb(test_id : int):
-	var succ : Array[ETestState] = [0]
-
-	var timer1 = GDRx.StartTimespan(1.0)
-	var timer2 = GDRx.StartPeriodicTimer(0.5)
-	var ret = GDRx.ReturnValue(42)
-	var stream = timer1.pipe2(
-		GDRx.op.amb(ret),
-		GDRx.op.amb(timer2)
-	)
-
-	var res = RefValue.Set(0)
-	stream.subscribe(
-		func(v): res.v = v,
-		func(__): _fail(test_id, succ, 0),
-		func(): _assert(res.v == 42, test_id, succ)
-	)
-
-func _test_op_average(test_id : int):
-	var succ : Array[ETestState] = [0]
-	var stream = GDRx.FromRange(1, 101).pipe1(
-		GDRx.op.average()
-	)
-	stream.subscribe(func(avg : float): _assert(avg == 50.5, test_id, succ))
-
-func _test_op_buffer_count(test_id : int):
-	var succ : Array[ETestState] = [0, 0]
+	var test_results = {}
+	var test_names = self.tests.split(",")
+	for t in test_names:
+		test_results[t] = ETestState.SUCCESS
 	
-	var res = RefValue.Set([])
-	var stream = GDRx.FromRange(1, 101).pipe1(
-		GDRx.op.buffer_with_count(10)
-	)
-	stream.subscribe(
-		func(buf : Array):
-			res.v.append(buf),
-		func(__): _fail(test_id, succ, 0),
-		func():
-			var count = 0
-			for i in range(res.v.size()):
-				for j in range(res.v[i].size()):
-					count += res.v[i][j]
-			_assert(res.v.all(func(elem): return elem.size() == 10), test_id, succ, 0)
-			_assert(count == 5050, test_id, succ, 1)
-	)
-
-func _test_op_catch(test_id : int):
-	var succ : Array[ETestState] = [0, 0]
+	var tests = ArrayIterator.new(test_names)
+	var current_test = RefValue.Null()
+	var test_counter = RefValue.Set(0)
+	var n_tests = test_names.size()
 	
-	var res1 = RefValue.Set(false)
-	var stream1 = GDRx.Throw(GDRx.err.Error.new("Catched Error")).pipe1(
-		GDRx.op.catch(func(__, ___): return GDRx.ReturnValue(42))
-	)
-	stream1.subscribe(
-		func(i): res1.v = i == 42,
-		func(e): _fail(test_id, succ, 0),
-		func(): _assert(res1.v, test_id, succ, 0)
+	var print_results = func():
+		var n_passed = 0
+		var n_skipped = 0
+		var n_failed = 0
+		for t in test_names:
+			var res = "success"
+			if test_results[t] == ETestState.FAILED:
+				res = "failed"
+				n_failed += 1
+			elif test_results[t] == ETestState.MISSING:
+				res = "skipped"
+				n_skipped += 1
+			else:
+				n_passed += 1
+			print("[ReactiveX]: ", t, ": ", res)
+		print("\n[ReactiveX]: =====================")
+		print("[ReactiveX]: PASSED ", n_passed)
+		print("[ReactiveX]: FAILED ", n_failed)
+		print("[ReactiveX]: SKIPPED ", n_skipped)
+	
+	var next_test = func():
+		current_test.v = tests.next()
+		test_counter.v += 1
+		if current_test.v is tests.End:
+			print_results.call()
+			return
+		print("[ReactiveX]: Running test '", current_test, "' . . . (", test_counter, " / ", n_tests, ")")
+		var method = get("_test_" + current_test.v)
+		if method == null:
+			sequence_finished.emit(ETestState.MISSING, -1)
+		else:
+			method.call()
+	
+	GDRx.FromGodotSignal(self.sequence_finished).subscribe(
+		func(tup : Tuple):
+			var result : ETestState = tup.at(0)
+			var remaining : int = tup.at(1)
+			if result == ETestState.FAILED:
+				print("[ReactiveX]: FAILED\n")
+				test_results[current_test.v] = ETestState.FAILED
+				next_test.call()
+			elif result == ETestState.MISSING:
+				print("[ReactiveX]: SKIPPED\n")
+				test_results[current_test.v] = ETestState.MISSING
+				next_test.call()
+			elif remaining == 0:
+				print("[ReactiveX]: SUCCESS\n")
+				next_test.call()
 	)
 	
-	var stream2 = GDRx.Throw(GDRx.err.Error.new("Catched Error")).pipe1(
-		GDRx.op.catch(func(__, ___): return GDRx.err.Error.new("Uncatched Error"))
-	)
-	stream2.subscribe(
-		func(i): _fail(test_id, succ, 1),
-		func(e): _success(test_id, succ, 1),
-		func(): _fail(test_id, succ, 1)
-	)
+	next_test.call()
 
-func _test_op_combine_latest(id : int):
-	var succ : Array[ETestState] = [0, 0]
-	
-	var res = []
-	var stream = GDRx.FromRange(3).pipe1(
-		GDRx.op.combine_latest([GDRx.ReturnValue(42)])
-	)
-	stream.subscribe(
-		func(t : Tuple): res.append(t),
-		func(e): _fail(id, succ, 0),
-		func():
-			_assert(res.size() == 3 and res.all(func(elem): return elem.at(1) == 42), id, succ, 0)
-			var c = 0
-			for t in res:
-				c += t.at(0)
-			_assert(c == 6, id, succ, 1)
-	)
+static func UNSUB(v) -> ObservableSequence.UnsubscribeOn:
+	return ObservableSequence.UnsubscribeOn.new(v)
+var COMPLETE : ObservableSequence.Complete = ObservableSequence.Complete.new()
+var ERROR : ObservableSequence.Error = ObservableSequence.Error.new()
+
+# ============================================================================ #
+# 								Test Suit									   #
+# ============================================================================ #
+func _test_map():
+	var seq = ObservableSequence.new([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, COMPLETE])
+	var obs = GDRx.FromRange(10)
+	seq.compare(obs, self.sequence_finished)
