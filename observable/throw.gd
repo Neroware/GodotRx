2047b99614237c504b384028cde667d84dbb674e
Raoul Zebisch <neroware@freenet.de>
2022-09-30 14:11:23 +0200

    Fixed bug: Wrong scheduler selection in throw and empty

--- observable/throw.gd
+++ observable/throw.gd
@@ -1,7 +1,10 @@
 static func throw_(err, scheduler : SchedulerBase = null) -> Observable:
 	
-	var subscribe = func(observer : ObserverBase, scheduler : SchedulerBase = null) -> DisposableBase:
-		var _scheduler = scheduler if scheduler != null else ImmediateScheduler.singleton()
+	var subscribe = func(observer : ObserverBase, scheduler_ : SchedulerBase = null) -> DisposableBase:
+		var _scheduler : SchedulerBase
+		if scheduler != null: _scheduler = scheduler
+		elif scheduler_ != null: _scheduler = scheduler_
+		else: _scheduler = ImmediateScheduler.singleton()
 		
 		var action = func(scheduler : SchedulerBase, state):
 			observer.on_error(err)
