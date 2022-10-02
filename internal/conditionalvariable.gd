709fd23dd9ed4cab1940c34414de2e70445a4200
Raoul Zebisch <neroware@freenet.de>
2022-10-02 22:56:36 +0200

    Improved god-awful implementation of ConditionalVariable

--- internal/conditionalvariable.gd
+++ internal/conditionalvariable.gd
@@ -2,10 +2,10 @@ class_name ConditionalVariable
 
 ## Naive implementation of a conditional variable for multi-threading
 
-var _lock
-var _waiting_queue : Array
+var _lock : LockBase
+var _waiting_queue : Array[Lock]
 
-func _init(lock_ = null):
+func _init(lock_ : LockBase = null):
 	self._lock = lock_ if lock_ != null else RLock.new()
 	self._waiting_queue = []
 
@@ -19,36 +19,46 @@ func notify(n = 1):
 	if not self._lock.is_locking_thread():
 		GDRx.exc.LockNotAquiredException.new().throw()
 		return
-	for __ in range(n): self._waiting_queue.pop_front()
+	for __ in range(n):
+		if self._waiting_queue.is_empty():
+			break
+		var next : Lock = self._waiting_queue.pop_front()
+		next.unlock()
 
 func notify_all():
 	if not self._lock.is_locking_thread():
 		GDRx.exc.LockNotAquiredException.new().throw()
 		return
-	self._waiting_queue.clear()
+	while not self._waiting_queue.is_empty():
+		var next : Lock = self._waiting_queue.pop_front()
+		next.unlock()
 
-func wait(time_sec : float = -1.0) -> bool:
+func wait(time_sec = null) -> bool:
 	if not self._lock.is_locking_thread():
 		GDRx.exc.LockNotAquiredException.new().throw()
 		return false
-	var id = OS.get_thread_caller_id()
 	
-	var timeout_ = RefValue.Set(false)
-	if time_sec >= 0.0:
-		var timer = GDRx.get_tree().create_timer(time_sec)
-		timer.connect("timeout", func():
-			timeout_.v = true
-			for conn in timer.timeout.get_connections():
-				timer.timeout.disconnect(conn["callable"])
-		)
+	var thread_lock : Lock = Lock.new()
+	thread_lock.lock()
+	self._waiting_queue.push_back(thread_lock)
 	
-	if id in self._waiting_queue:
-		push_warning("Warning! Multiple wait-calls present for single thread!")
-	self._waiting_queue.push_back(id)
+	var notified = RefValue.Set(true)
 	
-	while id in self._waiting_queue and not timeout_.v:
-		self._lock._unlock_and_store_recursion_depth()
-		OS.delay_usec((randi() % 10) + 1)
-		self._lock._lock_and_restore_recursion_depth()
+	if time_sec != null:
+		var time_sec_ : float = time_sec
+		var timeout_interval = func():
+			OS.delay_msec(1000.0 * time_sec_)
+			self._lock.lock()
+			if thread_lock in self._waiting_queue:
+				thread_lock.unlock()
+				self._waiting_queue.erase(thread_lock)
+				notified.v = false
+			self._lock.unlock()
+		var timeout_thread = GDRx.concur.StartableThread.new(timeout_interval)
+		timeout_thread.start()
 	
-	return not timeout_.v
+	self._lock._unlock_and_store_recursion_depth()
+	thread_lock.lock()
+	self._lock._lock_and_restore_recursion_depth()
+	
+	return notified.v
