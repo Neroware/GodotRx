47ef10f24cce449d085209abc1afc4806358ca34
Raoul Zebisch <neroware@freenet.de>
2023-09-25 19:23:13 +0200

    Refactored EventLoopScheduler

---
+++ scheduler/catchscheduler.gd
@@ -0,0 +1,110 @@
+extends PeriodicScheduler
+class_name CatchScheduler
+
+## A scheduler with exception handling wrapping a [SchedulerBase]
+
+var _scheduler : SchedulerBase
+var _handler : Callable
+var _recursive_original : SchedulerBase
+var _recursive_wrapper : CatchScheduler
+
+## Wraps a scheduler, passed as constructor argument, adding exception
+## handling for scheduled actions. The handler should return [b]true[/b] to
+## indicate it handled the exception successfully. Falsy return values will
+## be taken to indicate that the exception should be escalated (raised by
+## this scheduler).
+## [br][br]
+## [b]Args:[/b] [br]
+## [code]scheduler[/code]: The scheduler to be wrapped. [br]
+## [code]handler[/code]: Callable to handle exceptions raised by wrapped scheduler.
+func _init(scheduler : SchedulerBase, handler : Callable):
+	super._init()
+	self._scheduler = scheduler
+	self._handler = handler
+	self._recursive_original = null
+	self._recursive_wrapper = null
+
+## Returns the current point in time (timestamp)
+func now() -> float:
+	return self._scheduler.now()
+
+## Schedule a new action for future execution
+func schedule(action : Callable, state = null) -> DisposableBase:
+	action = self._wrap(action)
+	return self._scheduler.schedule(action, state)
+
+## Schedule a new action for future execution in [code]duetime[/code] seconds.
+func schedule_relative(duetime, action : Callable, state = null) -> DisposableBase:
+	action = self._wrap(action)
+	return self._scheduler.schedule_relative(duetime, action, state)
+
+## Schedule a new action for future execution at [code]duetime[/code].
+func schedule_absolute(duetime, action : Callable, state = null) -> DisposableBase:
+	action = self._wrap(action)
+	return self._scheduler.schedule_absolute(duetime, action, state)
+
+## Schedule a periodic action for repeated execution every time
+## [code]period[/code] seconds have expired.
+func schedule_periodic(
+	period : float,
+	action : Callable,
+	state = null) -> DisposableBase:
+		var schedule_periodic = self._scheduler.get("schedule_periodic")
+		if schedule_periodic == null:
+			GDRx.exc.NotImplementedException.Throw()
+			return Disposable.new()
+		
+		var disp : SingleAssignmentDisposable = SingleAssignmentDisposable.new()
+		var failed : RefValue = RefValue.Set(false)
+		
+		var periodic = func(state = null):
+			if failed.v:
+				return null
+			var res = RefValue.Null()
+			if GDRx.try(func():
+				res.v = action.call(state)
+			) \
+			.catch("Exception", func(e):
+				failed.v = true
+				if not self._handler.call(e):
+					GDRx.raise(e)
+				disp.dispose()
+			) \
+			.end_try_catch():
+				return null
+			return res.v
+		
+		var scheduler : PeriodicScheduler = self._scheduler
+		disp.disposable = scheduler.schedule_periodic(period, periodic, state)
+		return disp
+
+func _clone(scheduler : SchedulerBase) -> CatchScheduler:
+	return CatchScheduler.new(scheduler, self._handler)
+
+func _wrap(action : Callable) -> Callable:
+	var parent : CatchScheduler = self
+	
+	var wrapped_action = func(self_ : SchedulerBase, state = null):
+		var res = RefValue.Null()
+		if GDRx.try(func():
+			res.v = action.call(parent._get_recursive_wrapper(self_), state)
+		) \
+		.catch("Exception", func(ex):
+			if not parent._handler.call(ex):
+				GDRx.raise(ex)
+		) \
+		.end_try_catch():
+			return Disposable.new()
+		return res.v
+	
+	return wrapped_action
+
+func _get_recursive_wrapper(scheduler : SchedulerBase) -> CatchScheduler:
+	if self._recursive_wrapper == null or self._recursive_original != scheduler:
+		self._recursive_original = scheduler
+		var wrapper = self._clone(scheduler)
+		wrapper._recursive_original = scheduler
+		wrapper._recursive_wrapper = wrapper
+		self._recursive_wrapper = wrapper
+	
+	return self._recursive_wrapper
---
+++ scheduler/eventloopscheduler.gd
@@ -0,0 +1,174 @@
+extends PeriodicScheduler
+class_name EventLoopScheduler
+
+## Creates an object that schedules units of work on a designated thread.
+
+var _is_disposed : bool
+var _thread_factory : Callable
+var _thread : StartableBase
+var _lock : Lock
+var _condition : ConditionalVariable
+var _queue : PriorityQueue
+var _ready_list : Array[ScheduledItem]
+
+var _exit_if_empty : bool
+
+var this
+
+func _init(
+	thread_factory : Callable = GDRx.concur.default_thread_factory,
+	exit_if_empty : bool = false
+):
+	super._init()
+	this = self
+	this.unreference()
+	
+	self._is_disposed = false
+	self._thread_factory = thread_factory
+	self._thread = null
+	self._lock = Lock.new()
+	self._condition = ConditionalVariable.new()
+	self._queue = PriorityQueue.new()
+	self._ready_list = []
+	
+	self._exit_if_empty = exit_if_empty
+
+## Schedule a new action for future execution
+func schedule(action : Callable, state = null) -> DisposableBase:
+	return self.schedule_absolute(self.now(), action, state)
+
+## Schedule a new action for future execution in [code]duetime[/code] seconds.
+func schedule_relative(duetime, action : Callable, state = null) -> DisposableBase:
+	duetime = max(DELTA_ZERO, duetime)
+	return self.schedule_absolute(self.now() + duetime, action, state)
+
+## Schedule a new action for future execution at [code]duetime[/code].
+func schedule_absolute(duetime, action : Callable, state = null) -> DisposableBase:
+	if self._is_disposed:
+		GDRx.exc.DisposedException.Throw()
+		return Disposable.new()
+	
+	var dt = duetime
+	var si : ScheduledItem = ScheduledItem.new(self, dt, state, action)
+	
+	if true:
+		var __ = LockGuard.new(self._lock)
+		if dt <= self.now():
+			self._ready_list.append(si)
+		else:
+			self._queue.enqueue(si)
+		self._condition.notify() # signal that a new item is available
+		self._ensure_thread()
+	
+	return Disposable.new(func(): si.cancel())
+
+## Schedule a periodic action for repeated execution every time
+## [code]period[/code] seconds have expired.
+func schedule_periodic(
+	period : float,
+	action : Callable,
+	state = null) -> DisposableBase:
+		if self._is_disposed:
+			GDRx.exc.DisposedException.Throw()
+			return Disposable.new()
+		
+		return super.schedule_periodic(period, action, state)
+
+## Checks if there is an event loop thread running.
+func _has_thread() -> bool:
+	var __ = LockGuard.new(self._lock)
+	return not self._is_disposed and self._thread != null
+
+## Ensures there is an event loop thread running. Should be called under the gate.
+func _ensure_thread():
+	if self._thread == null:
+		var _self = self
+		var thread_manager = GDRx.THREAD_MANAGER
+		
+		var _run = func():
+			var thread = _self._thread
+			_self.run()
+			thread_manager.finish(thread)
+		
+		var thread_ : StartableBase = self._thread_factory.call(_run)
+		if thread_:
+			self._thread = thread_
+			self._thread.start()
+		else:
+			GDRx.exc.BadArgumentException.Throw()
+
+## Event loop scheduled on the designated event loop thread. 
+## The loop is suspended/resumed using the condition which gets notified
+## by calls to Schedule or calls to dispose.
+func run():
+	var ready : Array[ScheduledItem] = []
+	
+	while true:
+		
+		var time : float
+		if true:
+			var __ = LockGuard.new(self._lock)
+			
+			# The notification could be because of a call to dispose. This
+			# takes precedence over everything else: We'll exit the loop
+			# immediately. Subsequent calls to Schedule won't ever create a
+			# new thread.
+			if self._is_disposed:
+				return
+			
+			# Sort the ready_list (from recent calls for immediate schedule)
+			# and the due subset of previously queued items.
+			time = self.now()
+			while not self._queue.is_empty():
+				var due = self._queue.peek().duetime
+				while not self._ready_list.is_empty() and due > self._ready_list[0].duetime:
+					ready.append(self._ready_list.pop_front())
+				if due > time:
+					break
+				ready.append(self._queue.dequeue())
+			while not self._ready_list.is_empty():
+				ready.append(self._ready_list.pop_front())
+		
+		# Execute the gathered actions
+		while not ready.is_empty():
+			var item = ready.pop_front()
+			if not item.is_cancelled():
+				item.invoke()
+		
+		# Wait for next cycle, or if we're done let's exit if so configured
+		if true:
+			var __ = LockGuard.new(self._lock)
+			
+			if not self._ready_list.is_empty():
+				continue
+			
+			elif not self._queue.is_empty():
+				time = self.now()
+				var item = self._queue.peek()
+				var seconds = item.duetime - time
+				if seconds > 0:
+					#print("timeout: ", seconds)
+					self._condition.wait_for(self._lock, seconds)
+			
+			elif self._exit_if_empty:
+				self._thread = null
+				return
+			
+			else:
+				self._condition.wait(self._lock)
+
+## Ends the thread associated with this scheduler. All
+## remaining work in the scheduler queue is abandoned.
+func dispose():
+	var __ = LockGuard.new(self._lock)
+	if not this._is_disposed:
+		this._is_disposed = true
+		this._condition.notify()
+
+func _notification(what):
+	if what == NOTIFICATION_PREDELETE:
+		this.dispose()
+
+## Transforms this scheduler into a [Disposable]
+func as_disposable() -> Disposable:
+	return Disposable.new(dispose)
