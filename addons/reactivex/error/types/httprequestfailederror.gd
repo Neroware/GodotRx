5bceac7de770e9b34167469e94db47c857402bfd
Raoul Zebisch <neroware@freenet.de>
2024-05-02 19:36:52 +0200

    Fixed wrong message output in HttpRequestFailedError

--- addons/reactivex/error/types/httprequestfailederror.gd
+++ addons/reactivex/error/types/httprequestfailederror.gd
@@ -10,9 +10,9 @@ var error_message : String
 func _init(url_ : String, error_code_ : int, error_message_ : String):
 	self.url = url_
 	self.error_code = error_code_
-	self.error_message = error_message
+	self.error_message = error_message_
 	var msg = "HTTP Request to host '" + self.url + "' failed with code " + str(self.error_code) \
-		+ "" if self.error_message.is_empty() else " (" + self.error_message + ")"
+		+ ("" if self.error_message.is_empty() else " (" + self.error_message + ")")
 	super._init(msg, ERROR_TYPE)
 
 static func raise(default = null, error_message : String = "", url : String = "", error_code : int = -1):
